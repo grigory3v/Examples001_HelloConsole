6d07687d80f0499573ca858bc54ed246a09b0804
Ellehp <Grigory3v@gmail.com>
2022-06-24 00:04:18 +0400

    Example006_basse

---
+++ Example006_basse/Program.cs
@@ -0,0 +1,16 @@
+﻿int gir1 = 1;
+int gir2 = 2;
+int gir3 = 6;
+int gir4 = 8;
+int gir5 = 4; 
+
+int max = 0;
+
+if (gir1 > max ) max = gir1;
+if (gir2 > max ) max = gir2;
+if (gir3 > max ) max = gir3;
+if (gir4 > max ) max = gir4;
+if (gir5 > max ) max = gir5;
+
+Console.Write("max = ");
+Console.WriteLine(max);

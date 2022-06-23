292a9cc7c833881c15cafc1f276189158aedfe1f
Ellehp <Grigory3v@gmail.com>
2022-06-23 23:39:59 +0400

    Example003 + rundom

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,11 @@
-﻿int namber1 = 31;
-int namber2 = 5;
-int result = namber1 + namber2;
-Console.WriteLine(result); 
+﻿int number1 = 31;
+int number2 = 5;
+int result = number1 + number2;
+Console.WriteLine(result);
+
+int number3 = new Random().Next(1,10); //123456789
+int number4 = new Random().Next(1,10); //123456789
+Console.WriteLine(number3 );
+Console.WriteLine(number4 );
+int result2 = number3 + number4;
+Console.WriteLine(result2); 
